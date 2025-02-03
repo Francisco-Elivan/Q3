b93234c0f3264da9981ec44060de90fc4a8d925d
Elivan <f48898@gmail.com>
2025-02-03 10:11:30 -0300

    pag static 01

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import fs from "fs";//já pertencem integrados no Node.js
 import express from "express";
 import cors from "cors";
 import router from "./router/routes.js";
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 
 const app = express()
 
-app.use(express.static("src/build"))
 const corsOptions = {
   origin: '*', // Permitir todas as origens (ou substitua pelo domínio específico)
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitido
-
+  
 };
 
 app.use(cors(corsOptions))
@@ -43,6 +43,18 @@ const options = {
 // Criar servidor HTTPS
 https.createServer(options, app).listen(3003, () => {
   console.log("Servidor rodando em https://192.168.0.114:3003");
+});
+
+// Ajuste para usar __dirname em módulos ES
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Servir arquivos estáticos da build do React
+app.use(express.static(path.join(__dirname, '/build')));
+
+// Capturar todas as rotas e redirecionar para o index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/build', 'index.html'));
 });
 
 //app.listen(3003, () => { console.log("server running, port 3003") })  
